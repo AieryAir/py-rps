04b9a9be3448768ca9327dda4c49327c4215a19e
AieryAir <104162923+AieryAir@users.noreply.github.com>
2024-05-03 23:25:21 +0800

    add user choices output

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 print ("Simple Rock Paper Scissors Game")
-print ("What do you throw?")
 print ("1. Rock")
 print ("2. Paper")
 print ("3. Scissors")
-input ()
+userChoice = input ("What do you throw?")
+
+if userChoice == 1:
+print ("1")
+
+elif userChoice == 2:
+print ("1")
+
+elif userChoice == 3:
+print ("1")
+
+else:
+print ("You did not select a choice properly.")
